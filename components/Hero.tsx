4c80965359c272e550772d941f5526df663a6f52
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2025-02-14 01:25:10 +0330

    create components (button ,  textanimation)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa'
 function Hero() {
     return (
         <div className='pb-20 pt-36'>
@@ -19,6 +22,17 @@ function Hero() {
                         <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
                             توسعه سایت فوق حرفه ای با next.js
                         </h2>
+                        <TextGenerateEffect
+                            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                            words="تبدیل ایده‌ها به تجربه‌های کاربری مفید"
+                        />
+
+                        <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+                            .یه برنامه نویس Next سلام. من علیرضام
+                        </p>
+                        <a href="#about">
+                            <MagicButton title="نمایش نمونه کارها" icon={<FaLocationArrow />} />
+                        </a>
                     </div>
                 </div>
 
