306e895e992c54890cae34b946a5873c88c82f0e
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2025-02-14 04:21:56 +0330

    styling design

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -78,7 +78,7 @@ export const BentoGridItem = ({
     return (
         <div
             className={cn(
-                // remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
+                // !remove p-4 rounded-3xl dark:bg-black dark:border-white/[0.2] bg-white  border border-transparent, add border border-white/[0.1] overflow-hidden relative
                 "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
                 className
             )}
@@ -103,7 +103,7 @@ export const BentoGridItem = ({
                 </div>
                 <div
                     className={`absolute right-0 -bottom-5 ${id === 5 && "w-full opacity-80"
-                        } `}
+                        }`}
                 >
                     {spareImg && (
                         <img
@@ -127,14 +127,14 @@ export const BentoGridItem = ({
                         "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10"
                     )}
                 >
-                    {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
-                    <div className="font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10">
+                    {/*//! change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
+                    <div className={`font-sans font-extralight md:max-w-32 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10 text-right`}>
                         {description}
                     </div>
-                    {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
-                    {/* remove mb-2 mt-2 */}
+                    {/* //!add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
+                    {/*//! remove mb-2 mt-2 */}
                     <div
-                        className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+                        className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10 ${id !== 6 ? "text-right" : "text-center"}`}
                     >
                         {title}
                     </div>
@@ -144,7 +144,7 @@ export const BentoGridItem = ({
 
                     {/* Tech stack list div */}
                     {id === 3 && (
-                        <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+                        <div className="flex gap-1 lg:gap-5 w-fit absolute -left-3 lg:-right-2">
                             {/* tech stack lists */}
                             <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
                                 {leftLists.map((item, i) => (
--- data/index.ts
+++ data/index.ts
@@ -8,7 +8,7 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "I prioritize client collaboration, fostering open communication ",
+    title: "صداقت و ساختن اعتماد شرط اول ماندگاری و حسن توجه شماست. بهم اهمیت میدیم.",
     description: "",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4  lg:min-h-[60vh]]",
     imgClassName: "w-full h-full",
@@ -18,7 +18,7 @@ export const gridItems = [
   },
   {
     id: 2,
-    title: "I'm very flexible with time zone communications",
+    title: "سعی میکنم برات زمان خالی کنم.متناسب با شرایط شما...",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
@@ -28,8 +28,8 @@ export const gridItems = [
   },
   {
     id: 3,
-    title: "My tech stack",
-    description: "I constantly try to improve",
+    title: "مهارتام چیه؟",
+    description: "همیشه در حال توسعه مهارتامم",
     className: "lg:col-span-2 md:col-span-3 md:row-span-2",
     imgClassName: "",
     titleClassName: "justify-center",
@@ -38,7 +38,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: "Tech enthusiast with a passion for development.",
+    title: "علاقه دارم تکنولوژی هایی که بورس میشه رو تست کنم.",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
@@ -49,8 +49,8 @@ export const gridItems = [
 
   {
     id: 5,
-    title: "Currently building a JS Animation library",
-    description: "The Inside Scoop",
+    title: "الان در حال ساخت همین سایت شخصی هستم.",
+    description: "تمام جزییات...",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
@@ -59,7 +59,7 @@ export const gridItems = [
   },
   {
     id: 6,
-    title: "Do you want to start a project together?",
+    title: "دوست داری یه پروژه با هم شروع و ادامش بدیم؟",
     description: "",
     className: "lg:col-span-2 md:col-span-3 md:row-span-1",
     imgClassName: "",
